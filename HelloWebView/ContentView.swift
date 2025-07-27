95894aa1e39694601438a3059d9c66d07d070602
wumacms <wumacms@qq.com>
2025-07-27 16:54:36 +0800

    Initial Commit

---
+++ HelloWebView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWebView
+//
+//  Created by devlink on 2025/7/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloWebView/HelloWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWebViewApp.swift
+//  HelloWebView
+//
+//  Created by devlink on 2025/7/27.
+//
+
+import SwiftUI
+
+@main
+struct HelloWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
